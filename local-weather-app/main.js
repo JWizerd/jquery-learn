76bbb686392950fc0259fbb80ce626cf8930ed4d
JWizerd <jwodke@outlook.com>
2016-09-05 16:02:13 -0600

    finished local weather app

--- local-weather-app/main.js
+++ local-weather-app/main.js
@@ -1,28 +1,49 @@
 // this application uses the Open Weather MAP API http://openweathermap.org/
 
 $(document).ready(function(){
-
-var APPID = '894c88eee3fbaf06bc0f1b75d7d026fa';	// Your Yahoo Application ID
-var DEG = 'c';	// c for celsius, f for fahrenheit
-var temp;
-var city;
-var icon;
-
-temp = $("#temp");
-city = $("#city");
-icon = $("#icon");
-
-var weather = {};
-weather.temp = "55";
-weather.city = "Loveland";
-weather.icon = "Cloud-Drizzle-Moon.svg";
-
-update(weather);
-
+  getWeatherByCoords(); // THIS GUY RUNS THE SHOW
 });
 
-function update(weather){
-  temp.innerHTML = weather.temp + "&deg;";
-  city.innerHTML = weather.city;
-  icon.src = "icons/" + weather.icon;
+function getWeatherByCoords(){
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    openWeatherApiCall(lat, long);
+    });
+  }
+  else {
+    alert("We're sorry but you do not have navigator installed on your browser. This app will not work for you.");
+  }
+}
+
+function openWeatherApiCall(latitude, longitude) {
+  var APPID = '894c88eee3fbaf06bc0f1b75d7d026fa';
+  var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude +
+  "&lon=" + longitude + "&units=imperial&APPID=" + APPID;
+
+  $.get(url, function(data) {
+    $("#icon").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+    $("#temp").append(data.main.temp + "&deg;");
+    $("#city").append(data.name);
+    $("#description").append(data.weather[0].description);
+
+    // change background if temp is high low or med
+    backgroundImagesByTemp(data);
+  }).fail(function(){
+    alert("FAILURE");
+  });
+}
+
+function backgroundImagesByTemp(data){
+  var temperature = data.main.temp;
+  if (temperature < 45) {
+    $("body").css("background-image", "url('images/cold.jpg')");
+  }
+  else if (temperature > 45 && temperature < 80) {
+    $("body").css("background-image", "url('images/warm.jpg')");
+  }
+  else {
+    $("body").css("background-image", "url('images/hot.jpg')");
+  }
 }
